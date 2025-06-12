2d92ff65b070a8ac66bfd8d7d26aa40ec519048a
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-06-12 20:40:41 +0300

    Update ThezEnv.java

--- src/Thezsia/content/Thezsia/blocks/ThezEnv.java
+++ src/Thezsia/content/Thezsia/blocks/ThezEnv.java
@@ -13,7 +13,6 @@ public class ThezEnv{
             //environment
             basalticPatch, basalticRock, basalticWall,
             charrokFloor, hardCharrok, charrokPile, hardCharrokPile, charrokWall, hardCharrokWall, largeCharrokPile, largeHardCharrokPile,
-            limestone, limestoneDark, limestoneCracked, limestoneBoulder, limestoneDarkBoulder, limestoneWall, limestoneDarkWall, limestoneCrackWall,
             smoothSulfur, sulfurFloor, sulfurVent, sulfurBigVent, sulfurBoulder, sulfurWall, largeSulfurPile,
             peridotite, peridotiteDark, peridotiteCube, peridotiteDarkCube, peridotiteWall, peridotiteDarkWall, tritaniumCluster,
             //ores
@@ -26,37 +25,38 @@ public class ThezEnv{
                     attributes.set(Attribute.steam, 1f);
                 }};
                 */
+        //Basaltic, charrok biome
         basalticPatch = new Floor("basaltic-patch"){{
-            variants = 3;
+            variants = 5;
         }};
         basalticRock = new Prop("basaltic-rock"){{
-            variants = 2;
+            variants = 3;
         }};
         basalticWall = new StaticWall("basaltic-wall"){{
-            variants = 2;
+            variants = 4;
             basalticPatch.asFloor().wall = this;
             attributes.set(ThezAttribute.volcanicStone, 0.8f);
         }};
         charrokFloor = new Floor("charrok-floor"){{
-            variants = 2;
+            variants = 5;
         }};
         hardCharrok = new Floor("hard-charrok"){{
-            variants = 2;
+            variants = 5;
         }};
         charrokPile = new Prop("charrok-pile"){{
-            variants = 2;
+            variants = 3;
             charrokFloor.asFloor().decoration = this;
         }};
         hardCharrokPile = new Prop("hard-charrok-pile"){{
-            variants = 2;
+            variants = 3;
             hardCharrok.asFloor().decoration = this;
         }};
         charrokWall = new StaticWall("charrok-wall"){{
-            variants = 2;
+            variants = 4;
             charrokFloor.asFloor().wall = this;
         }};
         hardCharrokWall = new StaticWall("hard-charrok-wall"){{
-            variants = 2;
+            variants = 4;
             hardCharrok.asFloor().wall = this;
         }};
         largeCharrokPile = new TallBlock("large-charrok-pile"){{
@@ -67,40 +67,12 @@ public class ThezEnv{
             variants = 2;
             shadowAlpha = 0.3f;
         }};
-        limestone = new Floor("limestone"){{
-            variants = 2;
-        }};
-        limestoneDark = new Floor("limestone-dark"){{
-            variants = 2;
-        }};
-        limestoneCracked = new Floor("limestone-cracked"){{
-            variants = 2;
-        }};
-        limestoneBoulder = new Prop("limestone-boulder"){{
-            variants = 2;
-            limestone.asFloor().decoration = this;
-        }};
-        limestoneDarkBoulder = new Prop("limestone-dark-boulder"){{
-            variants = 2;
-            limestoneDark.asFloor().decoration = this;
-        }};
-        limestoneWall = new StaticWall("limestone-wall"){{
-            variants = 2;
-            limestone.asFloor().wall = this;
-        }};
-        limestoneDarkWall = new StaticWall("limestone-dark-wall"){{
-            variants = 2;
-            limestoneDark.asFloor().wall = this;
-        }};
-        limestoneCrackWall = new StaticWall("limestone-crack-wall"){{
-            variants = 2;
-            limestoneCracked.asFloor().wall = this;
-        }};
+        //Sulfur Biome
         smoothSulfur = new Floor("smooth-sulfur"){{
             variants = 4;
         }};
         sulfurFloor = new Floor("sulfur-floor"){{
-            variants = 4;
+            variants = 5;
             blendGroup = ThezEnv.smoothSulfur;
         }};
         sulfurVent = new SteamVent("sulfur-vent"){{
@@ -114,41 +86,42 @@ public class ThezEnv{
             attributes.set(Attribute.steam, 1.6f);
         }};
         sulfurBoulder = new Prop("sulfur-boulder"){{
-            variants = 2;
+            variants = 3;
             sulfurFloor.asFloor().decoration = smoothSulfur.asFloor().decoration = this;
         }};
         sulfurWall = new StaticWall("sulfur-wall"){{
-            variants = 2;
+            variants = 4;
             sulfurFloor.asFloor().wall = smoothSulfur.asFloor().wall = this;
         }};
         largeSulfurPile = new TallBlock("large-sulfur-pile"){{
             variants = 1;
             shadowAlpha = 0.3f;
         }};
+        //Peridotite biome
         peridotite = new Floor("peridotite"){{
-            variants = 4;
+            variants = 5;
         }};
         peridotiteDark = new Floor("peridotite-dark"){{
-            variants = 4;
+            variants = 5;
         }};
         peridotiteCube = new Prop("peridotite-cube"){{
-            variants = 2;
+            variants = 3;
             peridotite.asFloor().decoration = this;
         }};
         peridotiteDarkCube = new Prop("peridotite-dark-cube"){{
-            variants = 2;
+            variants = 3;
             peridotiteDark.asFloor().decoration = this;
         }};
         peridotiteWall = new StaticWall("peridotite-wall"){{
-            variants = 2;
+            variants = 4;
             peridotite.asFloor().wall = this;
         }};
         peridotiteDarkWall = new StaticWall("peridotite-dark-wall"){{
-            variants = 2;
+            variants = 4;
             peridotiteDark.asFloor().wall = this;
         }};
         tritaniumCluster = new TallBlock("tritanium-cluster"){{
-            variants = 2;
+            variants = 4;
             shadowAlpha = 0.3f;
         }};
         //ores
@@ -158,17 +131,17 @@ public class ThezEnv{
             wallOre = false;
         }}; */
         silverLick = new OreBlock("silver-lick"){{
-            variants = 3;
+            variants = 4;
             itemDrop = ThezItems.silver;
             wallOre = false;
         }};
         tritaniumOre = new OreBlock("tritanium-ore"){{
-            variants = 3;
+            variants = 4;
             itemDrop = ThezItems.tritaniumCrystal;
             wallOre = false;
         }};
         tritaniumOreWall = new OreBlock("tritanium-wallore"){{
-            variants = 3;
+            variants = 4;
             itemDrop = ThezItems.tritaniumCrystal;
             wallOre = true;
         }};
