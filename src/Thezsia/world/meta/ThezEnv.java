480a9b3c76e55204cddb23c18bdf476fa4b83e16
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-06-16 22:21:04 +0300

    NEW ENV!

---
+++ src/Thezsia/world/meta/ThezEnv.java
@@ -0,0 +1,10 @@
+package Thezsia.world.meta;
+
+import mindustry.world.meta.Env;
+
+/** Environmental flags for different types of locations. */
+public class ThezEnv extends Env {
+    public static final int
+    //is underwater, on a planet. But for my mod.
+    underwaterWarm = 1 << 14;
+}
