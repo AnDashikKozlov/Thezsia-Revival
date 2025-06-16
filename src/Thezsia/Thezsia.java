d6abc4ec884500185d80562cd9b52dfc30bc25de
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-06-16 22:14:17 +0300

    Update Thezsia.java

--- src/Thezsia/Thezsia.java
+++ src/Thezsia/Thezsia.java
@@ -4,6 +4,7 @@ import Thezsia.content.*;
 import Thezsia.content.Thezsia.ThezUnits;
 import Thezsia.content.Thezsia.blocks.ThezsiaBlocks;
 import Thezsia.world.blocks.ThezAttribute;
+import Thezsia.world.graphics.ThezEnvRenderers;
 import Thezsia.world.meta.*;
 import Thezsia.ui.*;
 import arc.*;
@@ -42,6 +43,7 @@ public class Thezsia extends Mod{
         ThezUnits.load();
         ThezsiaBlocks.load();
         ThezPlanets.load();
+        ThezEnvRenderers.init();
     }
 
 }
