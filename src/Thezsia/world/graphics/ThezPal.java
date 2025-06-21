29a7769f5a81a6cd7f4d680329c9021dc54e18b7
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-06-21 20:49:15 +0300

    Create ThezPal.java

---
+++ src/Thezsia/world/graphics/ThezPal.java
@@ -0,0 +1,33 @@
+package Thezsia.world.graphics;
+
+import arc.graphics.*;
+
+public class ThezPal{
+    public static Color
+    outlineCoreunitTree = Color.valueOf("212222"),
+    outlineIgnisTree = Color.valueOf("1f1c1c"),
+    outlineNoctisTree = Color.valueOf("242229"),
+    outlinePebbleTree = Color.valueOf("19181AFF"),
+
+    powerColorA = Color.valueOf("6c938e"),
+    powerColorB = Color.valueOf("ade7ec"),
+    powerColorC = Color.valueOf("8aede2"),
+
+    liquidLava = Color.valueOf("e06e4c"),
+    liquidMeltedTensorite = Color.valueOf("767ed6"),
+    liquidAmeba = Color.valueOf("51d285"),
+    gasOxygen = Color.valueOf("d3d8dd"),
+    gasAmmonia = Color.valueOf("c8a8fc"),
+    gasCarbonDioxide = Color.valueOf("767ed6"),
+    gasChlorine = Color.valueOf("bff270"),
+
+    itemTantalum = Color.valueOf("979faa"),
+    itemSilver = Color.valueOf("66bcc8"),
+    itemNihilite = Color.valueOf("666558"),
+    itemInfium = Color.valueOf("f5c05d"),
+    itemTensprite = Color.valueOf("67b5e9"),
+    itemSulfur = Color.valueOf("ffec70"),
+    itemTritaniumcrystal = Color.valueOf("caed6d"),
+    itemTritanium = Color.valueOf("5fe775")
+    ;
+}
