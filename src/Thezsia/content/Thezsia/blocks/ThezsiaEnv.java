1fcbe6e71fa3bc4bfc7d811977c567c5075a4946
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-06-29 23:30:31 +0300

    Update ThezsiaEnv.java

--- src/Thezsia/content/Thezsia/blocks/ThezsiaEnv.java
+++ src/Thezsia/content/Thezsia/blocks/ThezsiaEnv.java
@@ -9,6 +9,7 @@ import mindustry.world.blocks.environment.*;
 import mindustry.world.meta.Attribute;
 
 import static Thezsia.content.ThezItems.*;
+import static mindustry.content.Fx.none;
 
 public class ThezsiaEnv{
     public static Block
@@ -20,7 +21,7 @@ public class ThezsiaEnv{
             smoothSulfur, sulfurFloor, sulfurVent, sulfurBigVent, sulfurBoulder, sulfurWall, largeSulfurPile,
             peridotite, peridotiteDark, peridotiteCube, peridotiteDarkCube, peridotiteWall, peridotiteDarkWall, tritaniumCluster,
             //ores
-            basalticVein, silverLick, tritaniumOre, tritaniumOreWall;
+            nihiliteOre, silverLick, tritaniumOre, tritaniumOreWall;
     public static void load(){
         /*
                 testBiggerVent = new BiggerVent("test-bigger-vent"){{
@@ -30,42 +31,32 @@ public class ThezsiaEnv{
                 }};
                 */
         //Charrok, basaltic, magmatic biomes
-        charrokFloor = new Floor("charrok-floor"){{
-            variants = 5;
+        charrokFloor = new Floor("charrok-floor"){{variants = 5;
         }};
-        hardCharrok = new Floor("hard-charrok"){{
-            variants = 5;
+        hardCharrok = new Floor("hard-charrok"){{variants = 5;
         }};
-        charrokPile = new Prop("charrok-pile"){{
-            variants = 3;
+        charrokPile = new Prop("charrok-pile"){{variants = 3;
             charrokFloor.asFloor().decoration = this;
         }};
-        hardCharrokPile = new Prop("hard-charrok-pile"){{
-            variants = 3;
+        hardCharrokPile = new Prop("hard-charrok-pile"){{variants = 3;
             hardCharrok.asFloor().decoration = this;
         }};
-        charrokWall = new StaticWall("charrok-wall"){{
-            variants = 4;
+        charrokWall = new StaticWall("charrok-wall"){{variants = 4;
             charrokFloor.asFloor().wall = this;
         }};
-        hardCharrokWall = new StaticWall("hard-charrok-wall"){{
-            variants = 4;
+        hardCharrokWall = new StaticWall("hard-charrok-wall"){{variants = 4;
             hardCharrok.asFloor().wall = this;
         }};
-        largeCharrokPile = new TallBlock("large-charrok-pile"){{
-            variants = 3;
+        largeCharrokPile = new TallBlock("large-charrok-pile"){{variants = 3;
             shadowAlpha = 0.8f;
         }};
-        largeHardCharrokPile = new TallBlock("large-hard-charrok-pile"){{
-            variants = 3;
+        largeHardCharrokPile = new TallBlock("large-hard-charrok-pile"){{variants = 3;
             shadowAlpha = 0.8f;
         }};
 
-        basalticPatch = new Floor("basaltic-patch"){{
-            variants = 5;
+        basalticPatch = new Floor("basaltic-patch"){{variants = 5;
         }};
-        basalticRock = new Prop("basaltic-rock"){{
-            variants = 3;
+        basalticRock = new Prop("basaltic-rock"){{variants = 3;
             basalticPatch.asFloor().decoration = this;
         }};
         basalticWall = new StaticWall("basaltic-wall"){{
@@ -86,79 +77,65 @@ public class ThezsiaEnv{
         }};
         magmaOverlay = new OverlayFloor("magma-overlay"){{variants = 4;}};
 
-        //Sulfur Biome
-        smoothSulfur = new Floor("smooth-sulfur"){{
-            variants = 5;
+        //Sulfur biome
+        smoothSulfur = new Floor("smooth-sulfur"){{variants = 5;
         }};
-        sulfurFloor = new Floor("sulfur-floor"){{
-            variants = 5;
+        sulfurFloor = new Floor("sulfur-floor"){{variants = 5;
             blendGroup = smoothSulfur;
         }};
         sulfurVent = new SteamVent("sulfur-vent"){{variants = 2;
+            effect = none;
             blendGroup = parent = smoothSulfur;
             attributes.set(Attribute.steam, 1f);
         }};
         sulfurBigVent = new BiggerVent("sulfur-big-vent"){{variants = 2;
+            effect = none;
             blendGroup = parent = smoothSulfur;
             attributes.set(Attribute.steam, 1.6f);
         }};
-        sulfurBoulder = new Prop("sulfur-boulder"){{
-            variants = 3;
+        sulfurBoulder = new Prop("sulfur-boulder"){{variants = 3;
             sulfurFloor.asFloor().decoration = smoothSulfur.asFloor().decoration = this;
         }};
-        sulfurWall = new StaticWall("sulfur-wall"){{
-            variants = 4;
+        sulfurWall = new StaticWall("sulfur-wall"){{variants = 4;
             sulfurFloor.asFloor().wall = smoothSulfur.asFloor().wall = this;
         }};
-        largeSulfurPile = new TallBlock("large-sulfur-pile"){{
-            variants = 1;
+        largeSulfurPile = new TallBlock("large-sulfur-pile"){{variants = 1;
             shadowAlpha = 0.5f;
         }};
         //Peridotite biome
-        peridotite = new Floor("peridotite"){{
-            variants = 5;
+        peridotite = new Floor("peridotite"){{variants = 5;
         }};
-        peridotiteDark = new Floor("peridotite-dark"){{
-            variants = 5;
+        peridotiteDark = new Floor("peridotite-dark"){{variants = 5;
         }};
-        peridotiteCube = new Prop("peridotite-cube"){{
-            variants = 3;
+        peridotiteCube = new Prop("peridotite-cube"){{variants = 3;
             peridotite.asFloor().decoration = this;
         }};
-        peridotiteDarkCube = new Prop("peridotite-dark-cube"){{
-            variants = 3;
+        peridotiteDarkCube = new Prop("peridotite-dark-cube"){{variants = 3;
             peridotiteDark.asFloor().decoration = this;
         }};
-        peridotiteWall = new StaticWall("peridotite-wall"){{
-            variants = 4;
+        peridotiteWall = new StaticWall("peridotite-wall"){{variants = 4;
             peridotite.asFloor().wall = this;
         }};
-        peridotiteDarkWall = new StaticWall("peridotite-dark-wall"){{
-            variants = 4;
+        peridotiteDarkWall = new StaticWall("peridotite-dark-wall"){{variants = 4;
             peridotiteDark.asFloor().wall = this;
         }};
-        tritaniumCluster = new TallBlock("tritanium-cluster"){{
-            variants = 4;
+        tritaniumCluster = new TallBlock("tritanium-cluster"){{variants = 4;
             shadowAlpha = 0.3f;
         }};
         //ores
-        basalticVein = new OreBlock("basaltic-vein"){{
-            variants = 3;
+        nihiliteOre = new OreBlock("nihilite-ore"){{variants = 4;
             itemDrop = nihilite;
             wallOre = false;
         }};
-        silverLick = new OreBlock("silver-lick"){{
-            variants = 4;
+        silverLick = new OreBlock("silver-lick"){{variants = 4;
             itemDrop = ThezItems.silver;
             wallOre = false;
         }};
-        tritaniumOre = new OreBlock("tritanium-ore"){{
-            variants = 4;
+        tritaniumOre = new OreBlock("tritanium-ore"){{variants = 4;
             itemDrop = ThezItems.tritaniumCrystal;
             wallOre = false;
         }};
-        tritaniumOreWall = new OreBlock("tritanium-wallore"){{
-            variants = 4;
+        tritaniumOreWall = new OreBlock("tritanium-wallore"){{variants = 4;
             itemDrop = ThezItems.tritaniumCrystal;
             wallOre = true;
         }};
