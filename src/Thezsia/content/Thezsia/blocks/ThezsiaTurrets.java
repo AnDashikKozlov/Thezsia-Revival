77257a82abdb78981c59afbb83dd867437acc58b
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-07-13 03:47:58 +0300

    Update ThezsiaTurrets.java
    //TODO

--- src/Thezsia/content/Thezsia/blocks/ThezsiaTurrets.java
+++ src/Thezsia/content/Thezsia/blocks/ThezsiaTurrets.java
@@ -1,15 +1,22 @@
 package Thezsia.content.Thezsia.blocks;
 
+import Thezsia.type.bullet.PosLightningType;
+import Thezsia.world.graphics.ThezPal;
 import Thezsia.world.meta.ThezEnv;
 import arc.graphics.Color;
+import mindustry.Vars;
 import mindustry.content.Fx;
-import mindustry.entities.bullet.*;
-import mindustry.entities.part.*;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.entities.bullet.MissileBulletType;
+import mindustry.entities.part.HaloPart;
+import mindustry.entities.part.RegionPart;
+import mindustry.entities.part.ShapePart;
 import mindustry.gen.Sounds;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
-import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
+import mindustry.world.blocks.defense.turrets.PowerTurret;
 import mindustry.world.consumers.ConsumeCoolant;
 import mindustry.world.draw.DrawTurret;
 import mindustry.world.meta.Env;
@@ -19,7 +26,7 @@ import static Thezsia.content.ThezItems.*;
 public class ThezsiaTurrets{
     public static Block
             //Blocks
-            ember, gaze;
+            ember, gaze, stinger;
     public static void load() {
         ember = new ItemTurret("ember"){{
             envEnabled = Env.underwater | ThezEnv.underwaterWarm | Env.oxygen;
@@ -28,7 +35,7 @@ public class ThezsiaTurrets{
             squareSprite = false;
             coolant = consume(new ConsumeCoolant(0.1f, true, true));
             rotateSpeed = 3.1f;
-            outlineColor = Color.valueOf("19181AFF");
+            outlineColor = ThezPal.outlineTurret;
             targetAir = false;
             targetGround = true;
             reload = 82f;
@@ -96,10 +103,10 @@ public class ThezsiaTurrets{
             minWarmup = 0.95f; shootWarmupSpeed = 0.03f;
             consumePower(5);
             rotateSpeed = 2.5f;
-            outlineColor = Color.valueOf("19181AFF");
+            outlineColor = ThezPal.outlineTurret;
             targetGround = true; targetAir = false;
             reload = 4;
-            range = 180;
+            range = 25f * Vars.tilesize;
             fogRadiusMultiplier = 0.35f;
             inaccuracy = 45;
             velocityRnd = 0.2f;
@@ -191,6 +198,51 @@ public class ThezsiaTurrets{
                         }}
                 );
             }};
+        }};
+        stinger = new ItemTurret("stinger"){{
+            envEnabled = Env.underwater | ThezEnv.underwaterWarm | Env.oxygen;
+            requirements(Category.turret, ItemStack.with(tantalum, 110, silver, 40, infium, 75));
+            size = 3;
+            squareSprite = false;
+            coolant = consume(new ConsumeCoolant(0.25f, true, true));
+            rotateSpeed = 2.1f;
+            outlineColor = ThezPal.outlineTurret;
+            targetAir = false;
+            targetGround = true;
+            reload = 1.3f * 60f;
+            range = 18f * Vars.tilesize;
+            fogRadiusMultiplier = 0.25f;
+            inaccuracy = 0.2f;
+            shootY = 2;
+            shootSound = Sounds.laser;
+            //cooldownTime = 90f;
+            ammoPerShot = 5;
+            //minWarmup = 0.5f;
+            //shootWarmupSpeed = 0.03f;
+
+            ammo(
+                    tensorite, new PosLightningType(38){{
+                        lightningColor = hitColor = ThezPal.itemTensprite;
+                        shootSound = Sounds.spark;
+                        boltNum = 3;
+                        lightningDamage = 7;
+                        lightning = 3; lightningLength = 5; lightningLengthRand = 12;
+                        minRange = 5f * Vars.tilesize; maxRange = rangeOverride = 18f * Vars.tilesize;
+                        hitEffect = Fx.hitLaserColor;
+                        smokeEffect = Fx.greenCloud.wrap(ThezPal.itemTensprite);
+                    }},
+                    tritaniumCrystal, new PosLightningType(72){{
+                        lightningColor = hitColor = ThezPal.itemTritaniumcrystal;
+                        shootSound = Sounds.spark;
+                        boltNum = 5;
+                        lightningDamage = 21;
+                        lightning = 7; lightningLength = 12; lightningLengthRand = 19;
+                        minRange = 5f * Vars.tilesize; maxRange = rangeOverride = 25f * Vars.tilesize;
+                        hitEffect = Fx.hitLaserColor;
+                        smokeEffect = Fx.greenCloud.wrap(ThezPal.itemTritaniumcrystal);
+
+                    }}
+            );
         }};
     }
 }
