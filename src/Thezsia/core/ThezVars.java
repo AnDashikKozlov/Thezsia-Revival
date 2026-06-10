f283bc9fbaebd959958c2f0c4d477665d72aaf3a
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2026-06-10 19:10:53 +0300

    Added new settings (WIP)

--- src/Thezsia/core/ThezVars.java
+++ src/Thezsia/core/ThezVars.java
@@ -1,5 +1,6 @@
 package Thezsia.core;
 
+import arc.Core;
 import arc.struct.Seq;
 import mindustry.entities.units.BuildPlan;
 
@@ -11,4 +12,9 @@ public class ThezVars {
     /** Placement plans stored for wire bridging. DO NOT MODIFY! */
     public static Seq<BuildPlan> wirePlans = new Seq<>();
 
+    /** If true will hide some effects, like little smoke from cracked floor or magma. */
+    public static boolean  settingsLowDetail = Core.settings.getBool("@settings.lowdetail");
+
+    /** Testing / Debug mode. If True will give user access to any planet, etc. */
+    public static boolean  settingsDebug = Core.settings.getBool("@settings.debug");
 }
